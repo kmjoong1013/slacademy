ed39f9724eb0cbbfbec8367f0aab0e888e6a1f86
Administrator <Administrator@WIN7-20110816YW>
2019-07-03 11:59:00 +0900

    1

---
+++ basic/src/basic_syntax_ex/BasicGrammar.java
@@ -0,0 +1,19 @@
+package basic_syntax_ex;
+
+public class BasicGrammar {
+	
+	public static void main (String[] args) {
+		
+		int ±¹¾î=80;
+		int English=90;
+		int â¦ùÊ=75;
+		
+		int total = 0;
+		
+		total = ±¹¾î + English + â¦ùÊ;
+		
+		System.out.println("±¹¾î : "+±¹¾î);
+		System.out.println("English : "+English);
+		System.out.println("â¦ùÊ : "+â¦ùÊ);
+	}
+}
